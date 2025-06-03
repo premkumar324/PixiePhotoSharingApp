e4ad0a4cdbc916b3eadc64048ba44e24da6d024f
Premkumar lande <premkumarvitthallande@example.com>
2025-06-03 13:44:51 +0530

    finalfor now

--- src/components/PostGrid.jsx
+++ src/components/PostGrid.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import appwriteService from "../appwrite/config"
-import { FiHeart, FiMessageCircle } from 'react-icons/fi'
 
 function PostGrid({ post, onClick }) {
     return (
@@ -17,15 +16,10 @@ function PostGrid({ post, onClick }) {
             
             {/* Hover Overlay */}
             <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 flex items-center justify-center">
-                <div className="flex items-center space-x-6 opacity-0 group-hover:opacity-100 transition-opacity">
-                    <div className="flex items-center text-white">
-                        <FiHeart className="w-6 h-6" />
-                        <span className="ml-2 text-sm font-medium">0</span>
-                    </div>
-                    <div className="flex items-center text-white">
-                        <FiMessageCircle className="w-6 h-6" />
-                        <span className="ml-2 text-sm font-medium">0</span>
-                    </div>
+                <div className="opacity-0 group-hover:opacity-100 transition-opacity">
+                    <h3 className="text-white text-center font-medium px-4">
+                        {post.title}
+                    </h3>
                 </div>
             </div>
         </div>
